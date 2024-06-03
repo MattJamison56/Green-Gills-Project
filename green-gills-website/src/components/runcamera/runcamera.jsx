60196b3bde2b12809b1a2bd36cc5fb02db2cd9d2
Min Hyuk Choi <minhc3@gmail.com>
2024-06-03 15:51:03 -0700

    new component feature camera hide and show template

---
+++ green-gills-website/src/components/runcamera/runcamera.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const RunCamera = () => {
+  const [showIframe, setShowIframe] = useState(false);
+
+  const handleCameraToggle = () => {
+    setShowIframe(!showIframe);
+  };
+
+  return (
+    <div>
+      <button onClick={handleCameraToggle}>
+        {showIframe ? 'Hide Camera' : 'Show Camera'}
+      </button>
+      {showIframe && (
+        <iframe
+          src="http://192.168.0.236:8889/cam1/"
+          style={{ width: '100%', height: '500px', border: 'none', marginTop: '10px' }}
+          title="Camera Feed"
+        ></iframe>
+      )}
+    </div>
+  );
+};
+
+export default RunCamera;
