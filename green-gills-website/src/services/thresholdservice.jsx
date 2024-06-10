486c2b069cf78ec489c1f77730afd14f35957533
Min Hyuk Choi <minhc3@uci.edu>
2024-06-09 17:05:39 -0700

    Send Text message from Website

--- green-gills-website/src/services/thresholdservice.jsx
+++ green-gills-website/src/services/thresholdservice.jsx
@@ -4,6 +4,31 @@
 
 import { toast } from 'react-toastify';
 
+let vonage;
+async function initVonage() {
+  const { Vonage } = await import('@vonage/server-sdk');
+  vonage = new Vonage({
+    apiKey: "2edee5d5",
+    apiSecret: "5DEoUpVkMbt12cyx"
+  });
+}
+
+const from = "16105782839";
+const to = "15202257945";
+
+
+async function sendSMS(text) {
+  try {
+    const response = await vonage.sms.send({ to, from, text });
+    console.log('Message sent successfully');
+    console.log(response);
+  } catch (error) {
+    console.log('There was an error sending the messages.');
+    console.error(error);
+  }
+}
+
+
 export const checkThresholds = (data, thresholds, addNotification) => {
   Object.keys(thresholds).forEach(key => {
     const threshold = thresholds[key];
@@ -15,11 +40,18 @@ export const checkThresholds = (data, thresholds, addNotification) => {
         issue: `${key} alert: ${highValue[key]} at ${highValue.timestamp}`
       };
       addNotification(notification);
+
       if (!toast.isActive(`${key}Toast`)) {
         toast.warn(`${key} alert: ${highValue[key]} at ${highValue.timestamp}`, {
           toastId: `${key}Toast`
         });
       }
+
+      // Send SMS Text Notification
+      const text = `${key} alert: ${highValue[key]} at ${highValue.timestamp}`; 
+      sendSMS(text);
     }
   });
 };
+
+
